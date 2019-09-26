972148b4ed1fa3c37ed5fe94e325c463440d0299
Brian Gitego <gitegob7@gmail.com>
2019-09-26 14:32:36 +0200

    ft(sign up): employee can sign up
    - create a sign up route at "host/api/v1/auth/signup"
    - add a user model, data and validation
    - add JWT authentication
    
    [ Finishes #168694788 ]

---
+++ server/data/data.js
@@ -0,0 +1,36 @@
+export const users = [
+	{
+		id: '0',
+		firstName: 'Admin',
+		lastName: 'Brian',
+		email: 'gitegob@gmail.com',
+		password: '$2b$10$xqJZJRepBQBZ77Dh4LkQ1OeoLHdtIg/4FxyIshmvpw3xB9UYxeiZO',
+		gender: 'male',
+		jobROle: 'Software engineer',
+		department: 'engineering',
+		isAdmin: true
+	}
+];
+
+export const articles = [
+	{
+		id: 'article-0',
+		authoId: '0',
+		title: 'ONE IS BETTER THAN ONE',
+		article:
+      ' The most important thing you can learn from the Avengers is math: ONE is better than one. Meaning that one team is better than one person. This is very different than saying “five is better than one.”Sure, a group of 5 individuals can probably accomplish more than just one person alone…but it is only once that group of 5 becomes ONE that you see the magic happen. When you’re part of a team–a truly cohesive unit that functions with a single purpose–you can accomplish wonders. A single team can do more in a few days than one person can do in a month, or a bunch of individuals can do in a week.',
+		createdOn: '2018-Aug-20',
+		comments: [
+			{
+				id: 1,
+				comment: 'cool',
+				postedON: 'Sep 1st 2019, 10:00 am'
+			},
+			{
+				id: 2,
+				comment: 'inspiring',
+				postedOn: 'Sep 10th, 2:04 pm'
+			}
+		]
+	}
+];
---
+++ server/helpers/helper.js
@@ -0,0 +1,56 @@
+import jwt from 'jsonwebtoken';
+import { users, articles } from '../data/data';
+
+class Helper {
+	getToken({
+		id, email, firstName, lastName, isAdmin
+	}) {
+		const token = jwt.sign(
+			{
+				id,
+				email,
+				firstName,
+				lastName,
+				isAdmin
+			},
+			process.env.JWT_KEY
+		);
+		return token;
+	}
+
+	verifyToken(req, res, next) {
+		if (!req.headers.authorization) {
+			res.status(401).send({
+				status: 401,
+				error: 'Please log in or sign up first'
+			});
+		} else {
+			const token = req.headers.authorization.split(' ')[1];
+			try {
+				const decoded = jwt.verify(token, process.env.JWT_KEY);
+				req.payload = decoded;
+			} catch (error) {
+				return res.status(401).send({
+					status: 401,
+					error: 'Authentication failed'
+				});
+			}
+			const { email } = req.payload;
+			const match = users.find((el) => el.email === email);
+			if (!match) {
+				res.status(401).send({
+					status: 401,
+					error: 'Invalid token'
+				});
+			} else {
+				next();
+			}
+		}
+	}
+
+	findOne(id, where) {
+		return where.find((el) => `${el.id}` === id);
+	}
+}
+
+export default new Helper();
