7bd1a0a359cce72daf54545ac3d09bc735734412
Brian Gitego <gitegob7@gmail.com>
2019-10-07 21:58:08 +0200

    chore(database setup): Set up the database
    - re-organize the folders
    - install pg node library
    - write a config for the database
    
    [ Finishes #168971608 ]

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import userRoutes from './routes/userRoutes';
-import articleRoutes from './routes/articleRoutes';
+import userRoutes1 from './v1/routes/userRoutes';
+import articleRoutes1 from './v1/routes/articleRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,11 +22,14 @@ app.get('/', (_req, res) => {
 	res.status(200).send({
 		status: 200,
 		message:
-      'Welcome to TeamWork!, go to  https://documenter.getpostman.com/view/8741834/SVtPXARF?version=latest  or go to the REPO at https://github.com/gitego-brian/TeamWork for documentation'
+			'Welcome to TeamWork!, go to  https://documenter.getpostman.com/view/8741834/SVtPXARF?version=latest  or go to the REPO at https://github.com/gitego-brian/TeamWork for documentation'
 	});
 });
-app.use('/api/v1/auth', userRoutes);
-app.use('/api/v1/articles', articleRoutes);
+
+// VERSION ONE
+app.use('/api/v1/auth', userRoutes1);
+app.use('/api/v1/articles', articleRoutes1);
+
 
 // Error handling
 app.use('/*', (_req, res) => {
