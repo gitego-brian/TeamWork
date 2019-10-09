e115bdae981b6c40e1f2d2a00c2a8daade92a792
Brian Gitego <gitegob7@gmail.com>
2019-10-09 11:18:41 +0200

    ajustments in scripts

--- server/v2/database/dbConnect.js
+++ server/v2/database/dbConnect.js
@@ -10,21 +10,8 @@ const config = {
 	password: process.env.PG_PASSWORD,
 	port: process.env.PG_PORT
 };
-const configTest = {
-	user: process.env.PG_USER,
-	host: process.env.PG_HOST,
-	database: process.env.PG_TEST_DB,
-	password: process.env.PG_PASSWORD,
-	port: process.env.PG_PORT
-};
 // eslint-disable-next-line import/no-mutable-exports
-let pool;
-if (process.env.NODE_ENV === 'testing') {
-	pool = new Pool(configTest);
-	pool.connect(() => console.log('testing...'));
-} else {
-	pool = new Pool(config);
-	pool.connect(() => console.log('connected...'));
-}
+const pool = new Pool(config);
+pool.connect(() => console.log('connected...'));
 
 export default pool;
